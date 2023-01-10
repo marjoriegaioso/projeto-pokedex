1525ae7cbef819b06275fcc8df179ca870fe1622
marjoriegaioso <marjoriegaioso99@gmail.com>
2023-01-10 18:20:13 -0300

    poke-list

---
+++ pokedex/src/app/pages/pages.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//module
+import { RoutingModule } from './routing.module';
+//pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
